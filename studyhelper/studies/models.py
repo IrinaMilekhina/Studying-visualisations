801cb262939c09cd29c9ae03407be53a44971d76
RiNa <i.mezhova@gmail.com>
2024-04-19 15:57:35 +0200

    Create models

--- studyhelper/studies/models.py
+++ studyhelper/studies/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Activity(models.Model):
+    class Units(models.TextChoices):
+        LESSON = "L", "Lesson"
+        MINUTE = "M", "Minute"
+        SET = "S", "Set"
+        PAGE = "P", "Page"
+
+    unit = models.CharField(
+        max_length=1,
+        choices=Units.choices,
+        default=Units.MINUTE,
+    )
+
+    class Types(models.TextChoices):
+        CONSTANT = "CNS", "Constant"
+        COURSE = "CRS", "Course"
+        BOOK = "BOK", "Book"
+
+    type = models.CharField(
+        max_length=3,
+        choices=Types.choices,
+        default=Types.CONSTANT,
+    )
+    name = models.CharField(max_length=200)
+    number_of_lessons = models.IntegerField()
+
+
+class Logs(models.Model):
+    activity = models.ForeignKey(Activity, on_delete=models.CASCADE)
+    date = models.DateField(auto_now=False, auto_now_add=True)
+    quantity = models.IntegerField()
