b577f21c82df060de6bd308ed24bc79fee894c93
RiNa <i.mezhova@gmail.com>
2024-04-20 16:57:15 +0200

    add templates, views, urls and static

--- studyhelper/studies/views.py
+++ studyhelper/studies/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from studies.models import Activity
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the index.")
+
+
+def stats(request):
+    # Represent information about all studies
+    context = {'studies': Activity.objects.all()}
+
+    return render(request, 'studies/stats.html', context=context)
