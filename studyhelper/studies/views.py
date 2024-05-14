f4fcc873025caf12ce37326ec1619be780a22a55
RiNa <i.mezhova@gmail.com>
2024-05-14 20:39:46 +0200

    details

--- studyhelper/studies/views.py
+++ studyhelper/studies/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 
 from studies.models import Activity
 
@@ -10,6 +10,12 @@ def index(request):
 
 def stats(request):
     # Represent information about all studies
-    context = {'studies': Activity.objects.all()}
+    all_activities = get_list_or_404(Activity)
+    context = {'studies_objs': all_activities,
+               'number_of_studies': all_activities.count(),
+               'consonants': all_activities.filter(type="CNS").count(),
+               'courses': all_activities.filter(type="CRS").count(),
+               'books': all_activities.filter(type="BOK").count(),
+               'title': "Studies statistic page"}
 
     return render(request, 'studies/stats.html', context=context)
